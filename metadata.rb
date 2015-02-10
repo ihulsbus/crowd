7a2f25bd02ad0375e7a5fa25493893f5ba235e4d
Kleijn, M van der (SPLXK) - KLM <martijn.vanderkleijn@klm.com>
2015-02-10 13:15:03 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_crowd'
+maintainer       'KLM Royal Dutch Airlines'
+maintainer_email 'martijn.vanderkleijn@klm.com'
+license          ''
+description      'Installs/Configures chef_crowd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: chef_crowd
+# Recipe:: default
+#
+# Copyright 2015, KLM Royal Dutch Airlines
+#
