63555751ed640be8b3b154078346147cf47fe508
Kleijn, M van der (SPLXK) - KLM <Martijn-van-der.Kleijn@klm.com>
2015-07-28 14:00:13 +0200

    Fix placement of service creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,11 +15,10 @@ settings = Crowd.settings(node)
 include_recipe 'java'
 include_recipe 'crowd::database' if settings['database']['host'] == 'localhost'
 include_recipe "crowd::#{node['crowd']['install_type']}"
+include_recipe "crowd::#{node['crowd']['init_type']}"
 include_recipe 'crowd::configuration'
 
 # Install the proxy in front of Crowd, if any.
 unless node['crowd']['proxy'] == false
   include_recipe "crowd::#{node['crowd']['proxy']}"
 end
-
-include_recipe "crowd::#{node['crowd']['init_type']}"
