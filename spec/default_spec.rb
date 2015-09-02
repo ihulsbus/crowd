74ce926c3e43ddc9949ed6ad7cdc6938493c775b
Kleijn, M van der - KLM <martijn.vanderkleijn@klm.com>
2015-09-02 17:09:55 +0200

    Adding basic first ChefSpec test

---
+++ spec/default_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper.rb'
+
+describe 'crowd::default' do
+  before(:each) do
+    stub_command("ls /var/lib/postgresql/9.3/main/recovery.conf").and_return(false)
+    stub_command("cat /opt/atlassian/crowd/apache-tomcat/conf/server.xml | grep RemoteIpValve").and_return(false)
+    stub_command("which nginx").and_return('/usr/sbin/nginx')
+  end
+
+  let :chef_run do
+    ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04') do |node|
+      # Needed for testing postgresql cookbook
+      node.set['postgresql']['password']['postgres'] = 'pswpsw123'
+    end
+  end
+
+  it 'should include the apt::default recipe when platform is debian-based' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to include_recipe('apt::default')
+  end
+end
